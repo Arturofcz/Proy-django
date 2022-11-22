0b2aea887c26e5fbf91491cbdb687a0c3c4bfca1
arturo <arturofcz@gmail.com>
2022-11-21 19:10:53 -0500

    agregando urls

--- owner/views.py
+++ owner/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def owner_list(request):
+    data_context = {
+        'nombre_owner': 'kevin',
+        'edad': 18,
+        'pais': 'Peru',
+        'vigente': True
+
+    }
+
+    return render(request, 'owners.list.html', context={'data': data_context})
